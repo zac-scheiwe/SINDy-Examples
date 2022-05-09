f0b1111593ed1ff1d980340e2534f491452eebad
Zac Scheiwe <=>
2022-05-09 21:20:44 +1200

    not working yet

--- my_handy_functions.py
+++ my_handy_functions.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def reformat(feature_names):
     axis_nums = [1, 2]
     axis_names = ["x", "y"]
@@ -14,3 +16,17 @@ def print_result(model, solution_name):
     print("STLSQ model:")
     model.print()
 
+def lognormal_noisify(u, sd):
+    norm_sigma = np.sqrt(np.log(1+sd**2))
+    norm_mu = -norm_sigma**2/2
+    return(np.multiply(np.random.lognormal(norm_mu, norm_sigma, u.shape), u))
+
+# Test:
+# n = 100
+# abc = np.arange(1, 1+n**2).reshape([n,n])
+# efg = noisify(abc, 0.1)
+# ratios = np.divide(efg, abc)
+# np.mean(ratios), np.std(ratios)
+
+def noisify(u, sd):
+    return(u+np.random.normal(0, sd, u.shape))
